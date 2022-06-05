ef1d0efe165897da582f430e0fa60718dd94b7be
tyrellnote <ciel532@naver.com>
2022-06-05 18:22:56 +0900

    style

--- site/login.php
+++ site/login.php
@@ -7,17 +7,19 @@
     <title>Login</title>
 </head>
 <body>
+    <div class="login">
     <?php if(!isset($_session['login_id'])){ ?>
         <form method='post' action="login_ok.php " autocomplete="off">
         <p>Login Page</p><br>
-        <p><div class=subject>ID</div><br>
+        <p>ID<br>
         <p><input type="text" name='login_id' placeholder="아이디" maxlength="15"/></p>
-        <p><div class=subject>Password</div></div><br>
+        <p>Password<br>
         <p><input type="text" name='login_pw' placeholder="비밀번호" maxlength="20"/></p>
         <button type="submit" value="Login"> 로그인</button>
         <button type="button" value="join" onclick="window.location.href='./join.php'">회원가입</button>
         <button type="button" value="main" onclick="window.location.href='./main.php'">메인으로</button>
         </form>
+        
     <?php } else{
         $login_name = $_SESSION['name'];
         echo '<script type="text/javascript">';
--- site/main.php
+++ site/main.php
@@ -5,10 +5,13 @@
 <html lang="ko">
 
 <head>
-    <link rel="stylesheet" href="./style.css" ; <title>Yong site</title>
+    <div class="title">
+    <link rel="stylesheet" href="./style.css" ; <title>Tyrell</title>
+    </div>
 </head>
 
 <body>
+    <div class="center">
     <h1>메인 페이지 test</h1>
 
     <?php 
@@ -19,11 +22,14 @@
             echo "<p><button onclick=\"location.href='board.php'\">게시판</button></p>";
     }
     else{
+        
         echo "<p> 로그인이 필요합니다.</p><br>";
         echo "<p><button onclick=\"window.location.href='login.php'\">로그인
             </button> <button onclick=\"window.location.href='join.php'\">회원 가입 </button></p>";
-    }
+        
+        }
     ?>
+    </div>
 </body>
 
 </html>
